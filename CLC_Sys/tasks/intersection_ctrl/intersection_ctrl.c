7a457a0e08bd6a3a68ee2b9499aa7ff4370b8fe0
jaypacamarra <jpacamar97@gmail.com>
2022-01-01 21:41:45 -0800

    change to vTaskDelayUntil in intersection ctrl for flashing red light

--- CLC_Sys/tasks/intersection_ctrl/intersection_ctrl.c
+++ CLC_Sys/tasks/intersection_ctrl/intersection_ctrl.c
@@ -10,17 +10,13 @@ extern TIM_HandleTypeDef htim3;
 
 void vTask_IntersectionCtrl(void* params)
 {
-//	TickType_t xLastWakeTime;         // will hold the timestamp at which the task was last unblocked
-//	const TickType_t xFrequency = 5; // task frequency in ms
-//
-//	// Initialize the xLastWakeTime variable with the current time;
-//	xLastWakeTime = xTaskGetTickCount();
+	TickType_t xLastWakeTime;         // will hold the timestamp at which the task was last unblocked
+
+	// Initialize the xLastWakeTime variable with the current time;
+	xLastWakeTime = xTaskGetTickCount();
 
 	for(;;)
 	{
-		// Wait for the next cycle
-//		vTaskDelayUntil(&xLastWakeTime, xFrequency);
-
 		if ( state_machine_get_state() == CLC_train_crossing )
 		{
 			HAL_GPIO_TogglePin(LD5_GPIO_Port, LD5_Pin);	/* Red Led On*/
@@ -32,7 +28,7 @@ void vTask_IntersectionCtrl(void* params)
 
 			/* HAL_Delay and vTaskDelay same behaviour */
 //			HAL_Delay(750);
-			vTaskDelay(pdMS_TO_TICKS(750));
+			vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS(750) );
 		}
 		else
 		{
